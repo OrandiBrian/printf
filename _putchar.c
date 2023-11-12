991f6869133b98becf3706a3e58bab6a0969ad9a
Brian Orandi <brianorandi97@gmail.com>
2023-11-12 22:14:51 +0300

    Printf task 0

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to be printed
+ *
+ * Return: 1 on success
+ * On error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ print_%.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_% - prints %
+ *
+ * Return: 1
+ */
+
+int print_37(void)
+{
+	_putchar(37);
+
+	return (1);
+}
