c6ec352dade9539be3fb0fb99125c3620b843bb0
Brian Orandi <brianorandi97@gmail.com>
2023-11-13 14:00:22 +0300

    Printf Project

--- _printf.c
+++ _printf.c
@@ -1,48 +1,34 @@
 #include "main.h"
 
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
  */
 
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert p[] = {
-		{"%s", print_s}, {"%c", print_c},
-		{"%%", print_37},
-		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
-		{"%R", print_rot13}, {"%b", print_bin},
-		{"%u", print_unsigned},
-		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
-		{"%S", print_exc_string}, {"%p", print_pointer}
-	};
+	int printed = 0;
 
 	va_list args;
-	int i = 0, j, length = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-Here:
-	while (format[i] != '\0')
+	while (*format != '\0')
 	{
-		j = 13;
-		while (j >= 0)
+		if (*format == '%')
 		{
-			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
-			{
-				length += p[j].function(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			format++;
+			printed = selector(format, args, printed);
+			format++;
 		}
-		_putchar(format[i]);
-		length++;
-		i++;
+		else
+		{
+			_putchar(*format);
+			printed++;
+			format++;
+		}
 	}
 	va_end(args);
-	return (length);
+	return (printed);
 }
